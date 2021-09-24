5f0afb524a9ac59a3bb3788482fd03b6e9bc0f0f
Ali Roemhildt <aliroemhildt@gmail.com>
2021-09-24 14:25:31 -0600

    Declare dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  species: "Triceratop",
+  color: "Brown",
+  weightInTons: 10,
+  heightInFeet: 9.5,
+  isHerbivore: true,
+  isExtinct: true,
+  diet: ['grass', 'trees','shrubs'],
+  predators: ['Tyrannosaurus']
+};
