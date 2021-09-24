ac250a59dce4bdd2911b809ba29c2fa1d94a85e9
Ali Roemhildt <aliroemhildt@gmail.com>
2021-09-24 14:38:18 -0600

    Declare developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: 'Ali',
+  favoriteLanguage: 'JavaScript',
+  age: 24,
+  yearsOfExperience: 0,
+  hasGithubAccount: true,
+  usesMacbook: true,
+  currentlyLearning: ['HTML','CSS','JavaScript'],
+  learningResources: ['Turing School','Code Academy', 'W3 Schools', 'MDN Web Docs']
+};
