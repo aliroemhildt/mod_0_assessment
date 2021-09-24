cd28f766f48f6c6dc9d005b15ef0799826664507
Ali Roemhildt <aliroemhildt@gmail.com>
2021-09-24 14:09:17 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,12 @@
+var movie = "Dune";
+var isEntertaining = true;
+movie = "In the Heights";
+
+var num4 = 24;
+var num6 = 100;
+var product = num4 * num6;
+
+var favoriteFoods = ["Ramen", "Pizza", "Tacos"];
+favoriteFoods.shift();
+
+console.log(movie.length);
